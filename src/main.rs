e246c41906b534611783549d697e5f13dab51e20
4lineclear <tamimhakimi21@gmail.com>
2023-08-14 15:47:23 -0400

    Moved project to shuttle

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
 use actix_files::Files;
-use actix_web::{middleware::Logger, App, HttpServer};
+use actix_web::web::ServiceConfig;
+use shuttle_actix_web::ShuttleActixWeb;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-
-    log::info!("starting HTTP server at http://localhost:8080");
-
-    HttpServer::new(|| {
-        App::new()
-            .service(Files::new("/js", "static/js/").show_files_listing())
+#[shuttle_runtime::main]
+async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    let config = move |cfg: &mut ServiceConfig| {
+        cfg.service(Files::new("/js", "static/js/").show_files_listing())
             .service(Files::new("/img", "static/img/").show_files_listing())
             .service(Files::new("/css", "static/css/").show_files_listing())
-            .service(Files::new("/", "./static/html").index_file("index.html"))
-            .wrap(Logger::default())
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+            .service(Files::new("/", "./static/html").index_file("index.html"));
+    };
+
+    Ok(config.into())
 }
