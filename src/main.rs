fc6341a988150d3af06bc74fee13b7b77d0f6d5a
4lineclear <tamimhakimi21@gmail.com>
2023-08-14 15:24:46 -0400

    Restructured project

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,10 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
-            // We allow the visitor to see an index of the images at `/images`.
-            // .service(Files::new("/images", "static/images/").show_files_listing())
-            // Serve a tree of static files at the web root and specify the index file.
-            // Note that the root path should always be defined as the last item. The paths are
-            // resolved in the order they are defined. If this would be placed before the `/images`
-            // path then the service for the static images would never be reached.
-            .service(Files::new("/", "./static/root").index_file("index.html"))
-            // Enable the logger.
+            .service(Files::new("/js", "static/js/").show_files_listing())
+            .service(Files::new("/img", "static/img/").show_files_listing())
+            .service(Files::new("/css", "static/css/").show_files_listing())
+            .service(Files::new("/", "./static/html").index_file("index.html"))
             .wrap(Logger::default())
     })
     .bind(("127.0.0.1", 8080))?
